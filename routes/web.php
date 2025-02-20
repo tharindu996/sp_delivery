a6a3f6fc7fbb9766e70f35a66bddef1d52d3921a
“tharindu” <tharindudissanayake03@gmail.com>
2025-02-20 14:11:09 +0530

    add rate limitter

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\BaseController;
 use App\Http\Controllers\DeliveryRequestController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['throttle:15,1'])->group(function () {
+Route::middleware(['throttle:60,1'])->group(function () {
     Route::get('/', [BaseController::class, 'index'])->name('index');
+    Route::patch('/delivery-requests/{delivery_request}/cancel', [DeliveryRequestController::class, 'cancel'])->name('delivery-requests.cancel');
+    Route::resource('delivery-requests', DeliveryRequestController::class)->only(['index', 'create', 'store']);
 });
-Route::patch('/delivery-requests/{delivery_request}/cancel', [DeliveryRequestController::class, 'cancel'])->name('delivery-requests.cancel');
-Route::resource('delivery-requests', DeliveryRequestController::class)->only(['index', 'create', 'store']);
+    
